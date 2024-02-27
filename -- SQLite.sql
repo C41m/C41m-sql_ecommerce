80f32aefd4b5e56fdc359827c332df932cde087f
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-02-27 12:57:42 -0300

    Adiçao de Tasks_5 - Ciclo_03 - Concluído

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,7 +1,12 @@
 SELECT
   op.payment_type,
-  COUNT(op.order_id) AS pedidos
+  op.payment_installments,
+  MIN(op.payment_value) AS min_val_parc,
+  MAX(op.payment_value) AS max_val_parc,
+  AVG(op.payment_value) AS med_val_parc,
+  SUM(op.payment_value) AS som_val_parc
 FROM
-  order_payments op
+  orders o
 GROUP BY
-  op.payment_type
+  op.payment_type,
+  op.payment_installments
