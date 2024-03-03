34eb32e4124f74c7468916a77eb704e5d974ca6d
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-03-03 17:11:16 -0300

    Adiçao de Tasks_9 - Ciclo_06 - Em andamento.
    Conclusão Ciclo

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,17 +1,14 @@
 SELECT
-  o.order_id,
-  o.order_status,
-  oi.product_id,
-  p.product_category_name,
-  or2.review_score,
-  op.payment_value,
-  op.payment_type,
-  s.seller_city,
-  g.geolocation_lat,
-  g.geolocation_lng,
-  oi.seller_id,
-  s.seller_zip_code_prefix,
-  g.geolocation_zip_code_prefix
+  o.order_id AS id_pedido,
+  o.order_status AS status_pedido,
+  oi.product_id AS id_produto,
+  p.product_category_name AS categoria_produto,
+  or2.review_score AS nota_produto,
+  op.payment_value AS valor_pagamento,
+  op.payment_type AS tipo_pagamento,
+  s.seller_city AS cidade_vendedor,
+  g.geolocation_lat AS latitude,
+  g.geolocation_lng AS longitude
 FROM
   orders o
   LEFT JOIN order_items oi ON (oi.order_id = o.order_id)
