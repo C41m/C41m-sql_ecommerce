9240988724cf5a628456b7ba08eec23b65ccc510
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-03-02 23:14:29 -0300

    Adiçao de Tasks_9 - Ciclo_06 - Em andamento.

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,8 +1,19 @@
 SELECT
   o.order_id AS id_pedido,
-  o.customer_id AS id_cliente,
+  c.customer_state AS estado_cliente,
+  c.customer_city AS cidade_cliente,
   o.order_status AS status_pedido,
-  oi.product_id AS id_produto,
-  oi.price AS preco
+  p.product_category_name AS categoria_produto,
+  oi.price AS preco,
+  s.seller_city AS cidade_vendedor,
+  s.seller_state AS estado_vendedor,
+  op.payment_type AS tipo_pagamento
 FROM
-  orders o INNER JOIN order_items oi ON (oi.order_id = o.order_id)
+  orders o
+  INNER JOIN customer c ON (o.customer_id = c.customer_id)
+  INNER JOIN order_items oi ON (o.order_id = oi.order_id)
+  INNER JOIN products p ON (oi.product_id = p.product_id)
+  INNER JOIN sellers s ON (oi.seller_id = s.seller_id)
+  INNER JOIN order_payments op ON (o.order_id = op.order_id)
+WHERE
+  op.payment_type = "boleto"
